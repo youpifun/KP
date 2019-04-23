5e03e6fd918fa6478186fe007bded1e856203db6
Efim Konnov <youpifun@mail.ru>
2019-04-23 19:55:04 +0300

    First upload

--- index.php
+++ index.php
@@ -1,43 +1,29 @@
 <?php
-    session_start();
-    ?>
+session_start();
+?>
+<!DOCTYPE html>
 <html>
 <head>
-    <title>Main Page</title>
+<link rel="stylesheet" href="../css/single.css">
 </head>
+
 <body>
-<h2>Main Page</h2>
-<form action="handler.php" method="post">
-
-
-    <p>
-        <label>Your login:<br></label>
-        <input name="login" type="text" size="15" maxlength="15">
-    </p>
-
-    <p>
-
-        <label>Your password:<br></label>
-        <input name="password" type="password" size="15" maxlength="15">
-    </p>
-
-    <p>
-        <input type="submit" name="submit" value="Enter">
-
-
-        <br>
-
-        <a href="reg.php">Registration</a>
-    </p></form>
-<br>
-<?php
-    if (empty($_SESSION['login']) or empty($_SESSION['id']))
-    {
-    echo "You entered as guest<br><a href='#'> This ref is only for registered users</a>"; }
-else
-{
-echo "You entered as ".$_SESSION['login']."<br><a  href='http://google.com/'>This ref is only for registered users</a>";
-}
-?>
+		<header>
+			<nav>
+				<a href="catalog.html">Catalog</a> | <?php
+                if (empty($_SESSION['login']) or empty($_SESSION['id']))
+                {
+                    echo "You entered as guest"; }
+                else
+                {
+                    echo "You entered as ".$_SESSION['login'];
+                }
+                ?>
+			</nav>
+			<form>
+				<a href="main.php" class="auth-button">Login/Register
+				</a>
+			</form>
+		</header>
 </body>
 </html>
